2b2513c858385e7912df74dc8a11c0e7a2f495fe
yangjingqi <18179671772@163.com>
2022-01-26 00:03:40 +0800

    feat: 补充类型断言和gitigonre忽略文件

--- 08_typescript/03_类型补充/06_as类型断言.ts
+++ 08_typescript/03_类型补充/06_as类型断言.ts
@@ -8,14 +8,39 @@
 //  */
 //
 // myEl.src = '图片地址' //类型HTMLElement上不存在属性"src"
+//将一个相对普遍的类型转成比较具体的类型,从而能使用具体类型的某些方法
 
 /**
  *  类型为 'yang'
  */
-const name2 = 'yang'
+ const name2 = 'yang'
 
-const name = ('yang' as unknown) as number
-
-console.log(typeof name)
-
-export {}
+ const name = ('yang' as unknown) as number
+ 
+ console.log(typeof name)
+ 
+ /*  举例补充
+ * */
+ class Person {
+     teaching() {
+         console.log('教书')
+     }
+ }
+ 
+ class Student extends Person {
+     studying() {
+         console.log('学生的专属方法:学习')
+     }
+ }
+ 
+ function foo(user:Person){
+     // user.teaching() 有该方法
+     // user.studying()//Person类型上找不到该方法
+     (user as Student).studying()
+ }
+ 
+ const stu = new Student()
+ foo(stu)
+ 
+ 
+ export {}
