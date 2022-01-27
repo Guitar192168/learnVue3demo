4c885a8c513761eb7acf6cf85c9892b8641e044b
yangjingqi <yangjingqi@kungeek.com>
2022-01-27 18:26:44 +0800

    feat: 增加静态属性

--- 08_typescript/05_类Class/05_类的多态.ts
+++ 08_typescript/05_类Class/05_类的多态.ts
@@ -16,6 +16,7 @@ class Fish extends Animal {
     }
 }
 
+//继承是多态的前提
 //父类引用指向子类对象 const animal:Animal = new Dog()
 //多态可以编写通用性更强的代码，而不是用联合类型或者函数重载，反复书写
 function foo(animals: Animal[]) {
---
+++ 08_typescript/05_类Class/07_静态属性.ts
@@ -0,0 +1,8 @@
+class Student {
+    static age: string = "学生甲"
+}
+
+const stu = Student.age
+console.log(stu)
+
+export {}
