16d97aaffc9d65e95b645f4b251890025afc6706
yangjingqi <yangjingqi@kungeek.com>
2022-02-07 18:33:10 +0800

    feat: 补充vuex的state

--- 07_vuex/src/component/Home.vue
+++ 07_vuex/src/component/Home.vue
@@ -30,7 +30,8 @@ export default {
     fullName2: function () {
       return 'firstName' + 'lastName'
     },
-    ...mapState(['name', 'age'])
+    // 看起来像mapState, 其实还是读取了this.$store.state.name
+    // ...mapState(['name', 'age'])
   },
   setup() {
     // computed(() => {
@@ -42,12 +43,29 @@ export default {
     const sName = computed(() => store.state.name)
     const sAge = computed(() => store.state.age)
 
-    //这里如何将 storeInfo的属性 => 由function => 经过computed(function)包裹 => 返回ref对象
-    const storeInfo = mapState(['name', 'age'])
+    //这里如何将 storeState的属性 => 由function => 经过computed(function)包裹 => 返回ref对象
+
+    //{name:ref对象, age: ref } 再解构的时候就是将这些ref对象return了出去
+    const storeStateFns = mapState(['name', 'age'])
+
+    const stateStore = {}
+    Object.keys(storeStateFns).forEach(fnKey => {
+      //错误写法，还是对computed掌握不熟练
+      // stateStore[fnKey] = computed(() => store.state[storeStateFns[fnKey]])
+      const fn = storeStateFns[fnKey].bind({$store: store}) //这个参数是没有this的，this是undefined 取不到$store
+      //每个函数都有bind方法, 这里使用bind不使用call和apply是因为computed内部接收一个function
+      stateStore[fnKey] = computed(fn)//传进来的fn没有this
+
+      // stateStore[fnKey] = computed(storeStateFns[fnKey].bind({$store: store})) 不简写，可读性不强
+    })
+
+    console.log(stateStore)
     //这样写，相当于
-    const storeInfo2  = {
-      name:function (){},
-      age:function (){}
+    const storeState2 = {
+      name: function () {
+      },
+      age: function () {
+      }
     }
     //然后...结构出去，页面上就还是函数
 
@@ -56,7 +74,8 @@ export default {
       store,
       sName,
       sAge,
-      ...storeInfo
+      // ...storeState,
+      ...stateStore
     }
   }
 }
---
+++ 07_vuex/src/component/about.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+  import test from './index'
+
+  console.log(test)
+</script>
+
+<style scoped>
+
+</style>
