dab30eb1191b3d5b993411367b0d82709cca9dec
yangjingqi <18179671772@163.com>
2022-02-09 08:25:07 +0800

    feat: 迁移HOME内容到05组件

--- 07_vuex/src/pages/05_mapGetters.vue
+++ 07_vuex/src/pages/05_mapGetters.vue
@@ -10,16 +10,27 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import useState from "@/hooks/useState";
 
 export default {
   computed: {
-    ...mapGetters(['totalPrice']),
+    // ...mapGetters(['totalPrice']),
     //getters的对象写法与state的不同，不用写函数，直接写属性即可
     ...mapGetters({
       sTotalPrice: 'totalPrice'
     })
   },
+  setup() {
+    // const gettersInfo = mapGetters(['totalPrice'])
+    const gettersInfo = useState(['totalPrice'], mapGetters)
 
+    // return {
+    //   gettersInfo
+    // }
+    return {
+      ...gettersInfo
+    }
+  }
 }
 
 </script>
