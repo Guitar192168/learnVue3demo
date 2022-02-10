18413a7261a3bdabace21a4dfa0dec85f0651ad2
yangjingqi <yangjingqi@kungeek.com>
2022-02-10 20:43:35 +0800

    feat: 添加muatations

---
+++ 07_vuex/src/pages/05_mapGetters_封装.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{ totalPrice }}
+  </div>
+  <hr>
+  <div>
+    {{ sTotalPrice }}
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import useState from "@/hooks/useState";
+
+export default {
+  computed: {
+    // ...mapGetters(['totalPrice']),
+    //getters的对象写法与state的不同，不用写函数，直接写属性即可
+    ...mapGetters({
+      sTotalPrice: 'totalPrice'
+    })
+  },
+  setup() {
+    // const gettersInfo = mapGetters(['totalPrice'])
+    const gettersInfo = useState(['totalPrice'], mapGetters)
+
+    // return {
+    //   gettersInfo
+    // }
+    return {
+      ...gettersInfo
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 07_vuex/src/pages/06_muatations_mapMuatations.vue
@@ -0,0 +1,22 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <button @click="add">新增</button>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import {mapMutations} from 'vuex'-->
+
+<!--export default {-->
+<!--  name: "06_muatations",-->
+<!--  methods: {-->
+<!--    add(){-->
+<!--      this.$store.commit('increment')-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
