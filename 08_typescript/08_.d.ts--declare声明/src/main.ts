54f907ae1ea804cffaeef81c387e8b351afdc5eb
yangjingqi <yangjingqi@kungeek.com>
2022-02-11 17:04:27 +0800

    feat: 补充.d.ts

---
+++ 08_typescript/08_.d.ts--declare声明/src/main.ts
@@ -0,0 +1,11 @@
+import {sum} from './math'
+import axios from 'axios'
+
+//如果有被声明过，就不会报错了
+import _ from 'lodash'
+
+
+console.log(sum(1, 2))
+
+console.log(sum(10, 20))
+console.log(sum(10, 1000))
