0512e039c1a608e1bf4933b8d1e538cccc093bb3
yangjingqi <yangjingqi@kungeek.com>
2022-02-11 18:06:32 +0800

    feat: 补充.d.ts

--- 08_typescript/08_.d.ts--declare声明/src/main.ts
+++ 08_typescript/08_.d.ts--declare声明/src/main.ts
@@ -2,9 +2,13 @@ import {sum} from './math'
 import axios from 'axios'
 
 //如果有被声明过，就不会报错了
+//通过社区的一个公有库DefinitelyTyped存放类型声明文件
+//或者  npm i --save-dev @types/lodash
 import _ from 'lodash'
 
+console.log(_.join([1, 2, 3]))
 
+// document.getElementById()
 console.log(sum(1, 2))
 
 console.log(sum(10, 20))
