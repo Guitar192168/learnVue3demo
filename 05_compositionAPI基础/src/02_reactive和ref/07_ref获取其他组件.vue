1c29d44cec71e6a847b7b67b92f498acee614e06
yangjingqi <18179671776@163.com>
2022-05-06 17:25:31 +0800

    feat: 语法糖setup获取子组件的值需要用defineExpos

---
+++ 05_compositionAPI基础/src/02_reactive和ref/07_ref获取其他组件.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{}}</h2>
+  <button @click="show">查看组件的ref</button>
+  <hr>
+  <home ref="child"></home>
+
+</template>
+
+<script setup>
+import home from "./home.vue";
+import {ref} from 'vue'
+const child = ref(null)
+const show = () => {
+  console.log(child.value.name)
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 05_compositionAPI基础/src/02_reactive和ref/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    我是子组件
+    {{name}}
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const name = ref(123)
+defineExpose({
+  name
+})
+</script>
+
+<!--<script>-->
+<!--import {ref} from 'vue'-->
+
+<!--export default {-->
+<!--  setup() {-->
+<!--    const name = ref(123)-->
+<!--    return {-->
+<!--      name-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+
+<!--</script>-->
+<style scoped>
+
+</style>
