4bfc3e1a5154aea1fa8dfbfeab017dc52373ac6c
yangjingqi <18179671776@163.com>
2022-05-21 15:29:53 +0800

    fix: 补充vue2自定义事件input非输入框类型时使用model来区分，避免冲突

---
+++ 04_组件/zujian/src/views/04_vue2组件的v-model之非输入框/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input type="checkbox">
+
+        123
+
+        同样是自定义组件，如果子组件的input的type不是text, 而是checkbox。则一开始默认的
+        v-bind:value='变量'
+        v-on:input='事件'
+        则不通用了。
+        为了解决，可以使用model对象来避免冲突
+        model{
+        prop:'checked',
+        event:'change'
+        }
+
+        <child v-model="isChecked"></child>
+    </div>
+</template>
+
+<script>
+    import child from './child'
+
+    export default {
+        name: "App",
+        components: {
+            child
+        },
+        data() {
+            return {
+                isChecked: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 04_组件/zujian/src/views/04_vue2组件的v-model之非输入框/child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input type="checkbox" v-bind:checked="checked"  v-on:change="$emit('change', $event.target.checked)">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "child",
+        model: {
+            prop: 'checked',
+            event: 'change'
+        },
+        props: {
+            checked: Boolean
+        },
+        methods: {
+            childChange(e) {
+                // console.log(e.target.checked)
+                this.$emit('change', e)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
