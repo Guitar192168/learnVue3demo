11b26f189b8c82a6e6f56dde94c4514d5659b980
yangjingqi <18179671776@163.com>
2022-05-24 18:03:17 +0800

    feat: 补充vue2的指令

---
+++ 04_组件/src/views/17_vue2自定义指令/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-focus>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        directives: {
+            'focus': {
+                // bind不行
+                inserted: function (el) {
+                    // console.log(el.focus())
+                    el.focus()
+                },
+                componentUpdated(el, bindings, vNode, oldVnode) {
+                    console.log(bindings.value, 'vNode', vNode, 'oldVnode', oldVnode)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
