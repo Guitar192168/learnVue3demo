4eb1e2552b2bdf005524d6c14b16f87aa493a983
yangjingqi <18179671772@163.com>
2022-06-05 16:30:55 +0800

    feat: 提交自定义指令代码和Vue3源码部分

--- demo奶子哥/demo/src/main.js
+++
@@ -1,8 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-// import App from './Pagination'
-import router from './router'
-import store from './store'
-import loading from "./loading.js";
-
-createApp(App).use(store).use(router).mount('#app')
---
+++ demo奶子哥/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div style="height: 200px;width: 200px" v-loading="loading"></div>
+    <p v-loading="loading"></p>
+    检查是否是默认值
+    <Home></Home>
+
+  </div>
+
+</template>
+<script>
+import Home from './views/Home'
+
+export default {
+  components: {
+    Home
+  },
+  data() {
+    return {
+      loading: true
+    }
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
