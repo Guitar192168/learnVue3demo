6177275541b2a6a717a0998255d23ad430822ab1
yangjingqi <18179671776@163.com>
2022-06-30 17:30:31 +0800

    feat: 新增路由懒加载和魔法注释以及魔法注释不生效是因为上面通过import导入了组件，注释解决

--- 06_vue3版本的路由/src/router/index.js
+++ 06_vue3版本的路由/src/router/index.js
@@ -1,24 +1,43 @@
 // createWebHistory   createWebHashHistory
-import {createRouter, createWebHistory, createWebHashHistory} from 'vue-router'
-import Home from '../views/Home.vue'
+import {
+    createRouter,
+    createWebHistory,
+    createWebHashHistory
+} from 'vue-router'
+// !!!! 如果要用魔法注释，!!这里不能用import来导入组件
+// import Home from '../views/Home.vue'
+// import About from "../views/About";
 
+
+// 路由懒加载也可以这样写 然后赋值给home
+// const home = () => import('../views/Home.vue')
 
 // 配置映射对象
-const routes = [
-    {
+const routes = [{
         path: '/',
         // reLaunch:'/home',  // 错误写法
-        redirect:'/home', // 重定向到  '/home'
+        redirect: '/home', // 重定向到  '/home'
     },
     {
         path: '/home',
         name: 'Home',
-        component: Home
+        // component: Home
+        // component: () => import('../views/Home.vue')
+        // 魔法注释    打包的就可以有名字了
+        component: () => import( /* webpackChunkName:"Home" */ '../views/Home.vue'),
+        // 自定义的属性
+        meta: {
+            name: 'yjq',
+            age: 18
+        }
     },
     {
         path: '/about',
         name: 'About',
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        // component属性，可以接收一个函数，但是这个函数要求返回一个promise。  import('../').then(res => {console.log(res)})
+        // component: About
+        // component: () => import('../views/About.vue')
+        component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
     }
 ]
 
